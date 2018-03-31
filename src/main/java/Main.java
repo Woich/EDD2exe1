ea4ae429967757850e1271b5b368a83c87087bf6
Woich <minewoichbr@gmail.com>
2018-03-31 15:45:33 -0300

    Teste 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-
 import utfpr.dainf.ct.ed.exemplo.ArvoreBinaria;
 
 /**
@@ -15,7 +14,7 @@ import utfpr.dainf.ct.ed.exemplo.ArvoreBinaria;
  *               1   6     14 
  *                  / \    /
  *                 4   7  13
- * 
+ *     
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
  */
 public class Main {
@@ -28,7 +27,7 @@ public class Main {
         ArvoreBinaria<Integer> e = a.insereEsquerda(new ArvoreBinaria<>(3));
         e.insereEsquerda(new ArvoreBinaria<>(1));
         ArvoreBinaria<Integer> d = e.insereDireita(new ArvoreBinaria<>(6));
-        d.insereEsquerda(new ArvoreBinaria<>(4)).insereDireita(new ArvoreBinaria<>(5));
+        d.insereEsquerda(new ArvoreBinaria<>(4));
         d.insereDireita(new ArvoreBinaria<>(7));
 
         // monta o ramo direito
@@ -36,13 +35,39 @@ public class Main {
                 .insereDireita(new ArvoreBinaria<>(14))
                 .insereEsquerda(new ArvoreBinaria<>(13));
         
-        System.out.println("PERCURSO RECURSIVO");
+        System.out.println("\n\nPERCURSO RECURSIVO EM ORDEM");
         a.visitaEmOrdem();
-        System.out.println("\nPERCURSO ITERATIVO");
+        System.out.println("\nPERCURSO ITERATIVO EM ORDEM");
         ArvoreBinaria<Integer> no;
         while ((no = a.proximoEmOrdem()) != null) {
             System.out.print(" " + no.getValor());
         }
         
+        /*a.reinicia();
+        
+        System.out.println("\n\nPERCURSO RECURSIVO PRE ORDEM");
+        a.visitaPreOrdem();
+        System.out.println("\nPERCURSO ITERATIVO PRE ORDEM");
+        while ((no = a.proximoPreOrdem()) != null) {
+            System.out.print(" " + no.getValor());
+        }
+        
+        a.reinicia();
+        
+        System.out.println("\n\nPERCURSO RECURSIVO POS ORDEM");
+        a.visitaPosOrdem();
+        System.out.println("\nPERCURSO ITERATIVO POS ORDEM");
+        while ((no = a.proximoPosOrdem()) != null) {
+            System.out.print(" " + no.getValor());
+        }
+        
+        
+        a.reinicia();
+        
+        System.out.println("\nPERCURSO ITERATIVO EM NIVEL");
+        while ((no = a.proximoEmNivel()) != null) {
+            System.out.print(" " + no.getValor());
+        }*/
     }
 }
+
